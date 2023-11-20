0a1c88576d22f192b50d544c3555aecc143f4be4
Shaif Hossain <shossain@catec.ca>
2023-11-20 13:06:09 +0600

    udated

--- tests/integration/integration.test.js
+++ tests/integration/integration.test.js
@@ -1,4 +1,4 @@
-// tests/integration.test.js
+// tests/integration/integration.test.js
 const puppeteer = require('puppeteer');
 
 let browser;
@@ -9,9 +9,12 @@ beforeAll(async () => {
   page = await browser.newPage();
 });
 
-test('Integration Test Example', async () => {
-  await page.goto('http://your-app-url');
-  // Write your integration test logic here
+test('Example integration test for CSS styles', async () => {
+  // Test your CSS styles using Puppeteer
+  await page.goto(`file://${path.resolve(__dirname, 'src/index.html')}`);
+  const navBackgroundColor = await page.$eval('nav', (nav) => getComputedStyle(nav).backgroundColor);
+  expect(navBackgroundColor).toBe('rgb(27, 27, 27)');
+  // Add more CSS style assertions as needed
 });
 
 afterAll(async () => {
